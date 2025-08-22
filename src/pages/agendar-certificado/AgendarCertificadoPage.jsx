aecb0bebcea9c1bf90c9c3fc6cb20cc8bf236c8f
🚀mcoria🚀 <coria.marcelo.90@gmail.com>
2025-08-22 00:25:51 -0300

    feat: enhance AgendarCertificadoPage with decorative elements and additional content

--- src/pages/agendar-certificado/AgendarCertificadoPage.jsx
+++ src/pages/agendar-certificado/AgendarCertificadoPage.jsx
@@ -6,13 +6,19 @@ const AgendarCertificadoPage = () => {
   return (
     <>
       <div className="flex justify-between">
-        <div>AgendarCertificadoPage</div>
+        <div>⚡ AgendarCertificadoPage ⚡</div>
       </div>
       <div className="flex flex-row justify-center items-center mt-4">
         <EmailManager />
       </div>
       <div className="flex flex-row justify-center items-center mt-4">
         <EjemploCard />
+      </div>
+      <div>
+        <p className="text-center mt-4">
+          Esta página es un ejemplo de cómo se pueden integrar componentes personalizados
+          y manejar la lógica de envío de correos electrónicos en una aplicación React.
+        </p>
       </div>
     </>
   )
